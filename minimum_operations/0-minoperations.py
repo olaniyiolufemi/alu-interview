d53cd7202dcf0f2f3cc903f9254a99eb2550780e
olaniyiolufemi <o.olufemi@alustudent.com>
2024-09-14 22:39:34 +0200

    py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
+""" Minimum Operations"""
 
-def minOperation(n):
+
+def minOperations(n):
+    """ Minimum Operations"""
     if n <= 1:
         return 0
-    
-    num_of_operations = 0  # Initialize num_of_operations to 0
-    divisor = 2
-
-    while n > 1:
-        if n % divisor == 0:
-            n = n // divisor  # Use integer division (//) instead of float division (/)
-            num_of_operations += 1  # Increment num_of_operations by 1, not divisor
+    i = 2
+    result = 0
+    while i <= n:
+        if n % i == 0:
+            result += i
+            n = n / i
         else:
-            divisor += 1
-
-    return num_of_operations
-
-n = 12
-print(minOperation(n))
+            i += 1
+    return result
