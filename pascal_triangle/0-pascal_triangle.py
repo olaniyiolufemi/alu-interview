8acab87cac0ed2f811f07a46e628c69a0a06a726
olaniyiolufemi <o.olufemi@alustudent.com>
2024-10-03 23:18:29 +0200

    slash

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Create a function def pascal_triangle(n):
+that returns a list of lists of integers representing
+the Pascal triangle"""
+
+
+def pascal_triangle(n):
+    """function that returns a list of lists of integers"""
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
